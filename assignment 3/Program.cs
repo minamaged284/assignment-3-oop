3668be356b8b1534c4cceab66dbeb52dea3cb536
minamaged284 <minamina284@gmail.com>
2024-07-15 20:30:15 +0300

    Add project files.

---
+++ assignment 3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel;
+
+namespace assignment_3
+{
+    internal class Program
+    {
+        #region method overloading
+
+        /*
+         when more than a function has the same name but differs in parameters(type,number and order)
+         */
+        public static int sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int sum(int a, int b, int c)
+        {
+            return a + b;
+        }
+
+        public static int sum(int a, int b, int c , int d)
+        {
+            return a + b;
+        }
+
+        #endregion
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
